688ba2092dac01a95b659807e1b9dc49df44e9a1
Hold_Mine <95925492+Githoldi@users.noreply.github.com>
2023-10-07 00:47:13 +0200

    added a file in handlers/modals

---
+++ handlers/modals/modalTest.js
@@ -0,0 +1,15 @@
+import { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } from 'discord.js'
+import { usersWithPerm } from '../../utils/global.js'
+
+export default {
+  data: {
+    name: 'Modal Test',
+    description: '...',
+    id: 'modalid'
+  },
+  async execute(interaction) {
+
+    
+
+  }
+}
